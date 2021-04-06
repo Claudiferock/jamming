823c58476197ec86c999112f3767a7a83ef6d625
Claudio <claudifero@gmail.com>
2021-04-06 16:45:08 +0300

    Implement Spotify Access Token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,1 +1,31 @@
-  
+import { ClientId } from './clientId';
+
+const clientId = ClientId;
+const redirectURI = 'http://localhost:3000/';
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Acces Token', null, '/');
+
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessUrl;
+    }
+  }
+};
+
+export default Spotify;
